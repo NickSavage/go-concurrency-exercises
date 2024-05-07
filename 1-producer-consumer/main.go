abfffb69c9a419bd30c34365ca28fc7d81ab8a70
Nick Savage <nick@nicksavage.ca>
2024-05-06 20:27:20 -0400

    Solve exercise 1

--- 1-producer-consumer/main.go
+++ 1-producer-consumer/main.go
@@ -10,39 +10,60 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"sync"
 	"time"
 )
 
-func producer(stream Stream) (tweets []*Tweet) {
+var mu sync.Mutex
+
+func producer(stream Stream, tweets chan Tweet) {
+	defer close(tweets)
 	for {
 		tweet, err := stream.Next()
 		if err == ErrEOF {
-			return tweets
+			log.Printf("erro %v", err)
+			break
 		}
 
-		tweets = append(tweets, tweet)
+		tweets <- *tweet
 	}
 }
 
-func consumer(tweets []*Tweet) {
-	for _, t := range tweets {
-		if t.IsTalkingAboutGo() {
-			fmt.Println(t.Username, "\ttweets about golang")
-		} else {
-			fmt.Println(t.Username, "\tdoes not tweet about golang")
+func consumer(tweets <-chan Tweet) {
+	for {
+		tweet, ok := <-tweets
+		if !ok {
+			return
 		}
+		if tweet.IsTalkingAboutGo() {
+			fmt.Println(tweet.Username, "\ttweets about golang")
+		} else {
+			fmt.Println(tweet.Username, "\tdoes not tweet about golang")
+		}
+		//		time.Sleep(400 * time.Millisecond)
 	}
 }
 
 func main() {
 	start := time.Now()
 	stream := GetMockStream()
+	tweets := make(chan Tweet)
+	var wg sync.WaitGroup
 
+	wg.Add(2)
 	// Producer
-	tweets := producer(stream)
+	go func() {
+		defer wg.Done()
+		producer(stream, tweets)
+	}()
 
-	// Consumer
-	consumer(tweets)
+	go func() {
+		defer wg.Done()
+		consumer(tweets)
+	}()
+
+	wg.Wait()
 
 	fmt.Printf("Process took %s\n", time.Since(start))
 }
