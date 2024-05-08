b868df969ac1c4dde3fd3ec4d95febcd38c278ec
Nick Savage <nick@nicksavage.ca>
2024-05-07 21:14:14 -0400

    solve exercise 3

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,6 +10,13 @@
 
 package main
 
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+)
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
@@ -18,13 +25,47 @@ type User struct {
 	TimeUsed  int64 // in seconds
 }
 
+var mu sync.Mutex
+
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+	done := make(chan bool)
+	timeUp := make(chan bool)
+	var result bool
+
+	go func() {
+		go func() {
+			for {
+				mu.Lock()
+				if u.TimeUsed > 10 {
+					timeUp <- true
+				}
+				u.TimeUsed++
+				mu.Unlock()
+				time.Sleep(1 * time.Second)
+			}
+		}()
+		process()
+		done <- true
+	}()
+	select {
+
+	case <-done:
+		result = true
+	case <-timeUp:
+		log.Printf("times up")
+		result = false
+	case <-time.After(10 * time.Second):
+		log.Printf("process killed")
+		result = false
+	}
+	return result
 }
 
 func main() {
+
+	start := time.Now()
 	RunMockServer()
+	fmt.Printf("Process took %s\n", time.Since(start))
 }
